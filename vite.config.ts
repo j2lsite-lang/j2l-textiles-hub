b228bce001f8067568bc744c12eca64e577adbde
j2lsite-lang <j2lsite@gmail.com>
2026-01-28 17:24:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+      base: mode === "production" ? "/j2l-textiles-hub/" : "/",
   server: {
     host: "::",
     port: 8080,
